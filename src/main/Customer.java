f42b60d95f5dd0103a4b59489124334d1fb2aba7
Sebastian-Zok <sebastianpaulzok@gmail.com>
2022-05-16 17:29:21 +0200

    Renaming

--- src/main/Customer.java
+++ src/main/Customer.java
@@ -3,14 +3,14 @@ package main;
 import java.lang.*;
 import java.util.*;
 
-class Customer {
+public class Customer {
     private String name;
     private Vector rentals = new Vector();
     public Customer (String newname){
         name = newname;
     };
-    public void addRental(Rental arg) {
-        rentals.addElement(arg);
+    public void addRental(Rental aRental) {
+        rentals.addElement(aRental);
     };
     public String getName (){
         return name;
@@ -42,24 +42,24 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
-        double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+    private double amountFor(Rental aRental) {
+        double result = 0;
+        switch (aRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
-                thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+            	result += 2;
+                if (aRental.getDaysRented() > 2)
+                	result += (aRental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+            	result += aRental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
-                thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+            	result += 1.5;
+                if (aRental.getDaysRented() > 3)
+                	result += (aRental.getDaysRented() - 3) * 1.5;
                 break;
         }
-        return thisAmount;
+        return result;
     }
 
 }
